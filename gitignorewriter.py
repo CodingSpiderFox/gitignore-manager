129de9fe73b8030de1dffb89b9d6be4f4f8b9151
CodingSpiderFox <codingspiderfox@gmail.com>
2021-01-02 17:21:39 +0100

    Some small fixes

--- gitignorewriter.py
+++ gitignorewriter.py
@@ -1,14 +1,22 @@
+import os
+
+
 class GitignoreWriter:
 
     @staticmethod
     def write_result(target_path: str, source_file_names: [], repo_dir: str):
 
-        result_file_content : str = "################## gitignore-manager-contents below this line\r\n"
+        result_file_content: str = "################## gitignore-manager-contents below this line\r\n"
 
         for source_file_name in source_file_names:
             result_file_content += "################## " + source_file_name + " ##################\r\n"
             with open(repo_dir + "/Global/" + source_file_name, 'r') as file:
                 result_file_content += file.read() + "\r\n\r\n"
 
-        with open(target_path, 'a+') as file:
-            file.write(result_file_content)
+        if os.path.exists(target_path):
+            file_writing_mode = 'a+'  # append if already exists
+        else:
+            file_writing_mode = 'w'  # make a new file if not
+
+        with open(target_path, file_writing_mode) as target_file:
+            target_file.write(result_file_content)
--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import gitignoretemplatefetcher, gitignoreselector, gitignorewriter
 
 if __name__ == '__main__':
     repo_dir: str = gitignoretemplatefetcher.GitignoreTemplateFetcher.clone_repo()
-    source_file_names = gitignoreselector.GitignoreSelector.select_filenames(repo_dir)
-    gitignorewriter.GitignoreWriter.write_result("~/.global_gitignore_test", source_file_names, repo_dir)
+    source_file_names = gitignoreselector.GitignoreSelector.select_filenames(repo_dir).get("gitignores")
+    gitignorewriter.GitignoreWriter.write_result(".global_gitignore", source_file_names, repo_dir)
+    print("Check the result file \".gitignore_global\" and after creating a backup of the old one, manually copy it "
+          "to your home directory if it's correct.")
